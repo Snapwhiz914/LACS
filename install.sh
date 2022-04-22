a1803580d7f123c724cc782772ecb8d89ff3094f
Snapwhiz914 <64972906+Snapwhiz914@users.noreply.github.com>
2022-04-22 12:22:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ fi
 CONFIG_TEMPLATE=$'email: account@example.com
 password: AccountPassword123
 server: imap.server.com
+port: 143 #Default imap port, do not change unless necessary
 subject: "SubjectRequirement" #MAKE SURE THIS IS SECURE!
 time_in_hours: 24'
 touch /etc/lacs.yaml
